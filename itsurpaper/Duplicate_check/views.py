b063e81109f8e4062e239b5618182b05d3b9ea9d
nibikai <nibikai@baidu.com>
2024-07-15 17:45:40 +0800

    前端完善

--- itsurpaper/Duplicate_check/views.py
+++ itsurpaper/Duplicate_check/views.py
@@ -8,6 +8,9 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
 
 from mysql_script.mysql_op import *
+from init_database.ckg import *
+
+# from init_database import *
 
 
 def index(request):
@@ -23,26 +26,52 @@ def submit(request):
             "content": request.POST.get("content", ""),
         }
 
-        # TODO
-        
-        # 1. 计算simhash
-        # 2. 查询数据库
-        # 3. 返回结果
-        data["from"] = "test"
-
+        # 计算simhash
+        s_hash = shash(data["content"], data["title"])
         items = []
-        items.append(data)
-        context = {"items": items, "items_count": len(items)}
+        if len(s_hash) == 0:
+            return render(request, "no_result.html")
+        items = select_by_simhash([i["shash"] for i in s_hash])
+        # 返回结果
+        if len(items) == 0:
+            return render(request, "no_result.html")
+        context = {"items": items, "items_count": str(len(items))}
         return render(request, "result.html", context=context)
     else:
         return render(request, "index.html")  # 在非 POST 请求时返回 index 页面
 
 
-def shash(content: str):
-    # TODO 计算simhash
-    return 1515165153123123
+def shash(
+    content: str,
+    title: str,
+):
+    ckg = init(
+        content, title, 0, stop_words_path="Duplicate_check/static/stop_words.txt"
+    )
+    res = []
+    for i in ckg:
+        res.append({"content": i["content"], "shash": i["shash"]})
+    return res
 
 
-def select_by_simhash(shash: int):
-    # TODO 查询数据库
-    return [{"title": "test", "author": "test"}]
+def select_by_simhash(shashs: list) -> list:
+    res = mysql_select("corpus", {})
+    # TODO 多线程查询数据库
+
+    res = res[:10]
+    items = []
+    for i in res:
+        items.append(
+            {
+                "title": str(i[3]),
+                "author": str(i[4]),
+                "from": str(i[5]),
+                "content": str(i[2]),
+            }
+        )
+    return items
+
+
+def similarity(a, b):
+    # TODO 计算相似度
+    return 0
