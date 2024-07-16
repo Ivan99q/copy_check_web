01efb3028e9f6f03723c5bb609d67619b5ff4a42
. Ivan <595461760@qq.com.com>
2024-07-17 00:12:59 +0800

    v2.0开发中

--- itsurpaper/Duplicate_check/views.py
+++ itsurpaper/Duplicate_check/views.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
 from mysql_script.mysql_op import *
 from init_database.ckg import *
 
-# from init_database import *
+import multiprocessing
 
 
 def index(request):
@@ -26,68 +26,57 @@ def submit(request):
             "content": request.POST.get("content", ""),
         }
 
+        context = {
+            "author": data["author"],
+            "title": data["title"],
+            "original_content": data["content"].split("\n"),
+        }
+        context["original_content"].remove("\r")
+
         # 计算simhash
-        s_hash = shash(data["content"], data["title"])
+        s_hash = shash(data["content"])
         items = []
         if len(s_hash) == 0:
-            return render(request, "no_result.html")
-        items = select_by_simhash([i["shash"] for i in s_hash])
+            return render(request, "no_result.html", context=context)
+        items = select_by_simhash(s_hash)
         # 返回结果
         if len(items) == 0:
-            return render(request, "no_result.html")
-        context = {
-            "original_content": data["content"].split("\n"),
-            "items": items,
-            "items_count": str(len(items)),
-        }
+            return render(request, "no_result.html", context=context)
+        context["items"] = items
+        context["items_count"] = str(len(items))
         return render(request, "result.html", context=context)
     else:
         return render(request, "index.html")  # 在非 POST 请求时返回 index 页面
 
 
-def shash(
-    content: str,
-    title: str,
-):
-    ckg = init(
-        content, title, 0, stop_words_path="Duplicate_check/static/stop_words.txt"
+def shash(content: str) -> list:
+    res = init_with_unabled(
+        content, stop_words_path="Duplicate_check/static/stop_words.txt"
     )
-    res = []
-    for i in ckg:
-        res.append({"content": i["content"], "shash": i["shash"]})
+
     return res
 
 
-def select_by_simhash(shashs: list) -> list:
+def select_by_simhash(shashs: list) -> dict:
     # TODO 多线程查询数据库
 
-    res = {}
-    for shash in shashs:
-        # TODO 从数据库中获取相似度超过阈值的文段
+    # 阈值
+    thr = 0.92
 
-        # 阈值
-        thr = 0.92
+    def sub_select(shashs: list):
+        # 查询数据库
+        res = []
+        sql = """
+            SELECT * FROM corpus WHERE shash IN ()
+        """
+        res.append(mysql_exectute(sql))
 
-        this_res = mysql_select("corpus", {"shash": shash})
-
-        if len(this_res) > 0:
-            items = []
-            for i in this_res:
-                items.append(
-                    {
-                        "title": str(i[3]),
-                        "author": str(i[4]),
-                        "from": str(i[5]),
-                        "content": str(i[2]),
-                    }
-                )
-            res[shash] = items
-        else:
-            res[shash] = []
-    return items
+    with multiprocessing.Pool(processes=8) as pool:
+        res = dict()
+        pool.map(sub_select, shashs)
 
 
-def similarity(shash_a: str, shash_b: str) -> int:
+def similarity(shash_a: str, shash_b: str) -> float:
     # 计算相似度
     hanming = hammingDis(shash_a, shash_b)
     res = 1 - (float(hanming) / len(shash_a))
